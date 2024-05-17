b763652355554d5868ca2c8652177a52a35b18ce
0jsk <90405410+0jsk@users.noreply.github.com>
2024-05-17 18:50:36 +0300

    simple-tree: add eventrees method

--- src/main/java/ru/scndjk/dsa/SimpleTree/SimpleTree.java
+++ src/main/java/ru/scndjk/dsa/SimpleTree/SimpleTree.java
@@ -2,6 +2,20 @@ package ru.scndjk.dsa.SimpleTree;
 
 import java.util.*;
 
+class SimpleTreeNode<T> {
+    public T NodeValue; // значение в узле
+    public SimpleTreeNode<T> Parent; // родитель или null для корня
+    public List<SimpleTreeNode<T>> Children; // список дочерних узлов или null
+    public int Level;
+
+    public SimpleTreeNode(T val, SimpleTreeNode<T> parent) {
+        NodeValue = val;
+        Parent = parent;
+        Children = null;
+        Level = parent == null ? 0 : parent.Level + 1;
+    }
+}
+
 public class SimpleTree<T> {
     public SimpleTreeNode<T> Root;
 
@@ -125,6 +139,35 @@ public class SimpleTree<T> {
         for (SimpleTreeNode<T> child : node.Children) {
             setLevelsRecursively(child, level + 1);
         }
+    }
+
+    public ArrayList<T> EvenTrees() {
+        ArrayList<T> result = new ArrayList<>();
+        removeEdgesForEvenTrees(Root, result);
+        return result;
+    }
+
+    private int removeEdgesForEvenTrees(SimpleTreeNode<T> node, ArrayList<T> result) {
+        if (node == null) {
+            return 0;
+        }
+
+        int nodeCount = 1;
+
+        if (node.Children != null) {
+            for (SimpleTreeNode<T> child : node.Children) {
+                int childCount = removeEdgesForEvenTrees(child, result);
+
+                if (childCount % 2 == 0) {
+                    result.add(node.NodeValue);
+                    result.add(child.NodeValue);
+                }
+
+                nodeCount += childCount;
+            }
+        }
+
+        return nodeCount;
     }
 }
 
